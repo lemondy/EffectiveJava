53a0d2d408bf60980abaddb670b72a93426a3eda
lemondy <lemondy9@gmail.com>
2016-06-20 22:37:21 +0800

    add singleton

--- src/chapter2/Singleton.java
+++ src/chapter2/Singleton.java
@@ -1,5 +1,20 @@
 package chapter2;
 
 public class Singleton {
+	public static void main(String[] args) {
+		Foo f = Foo.INSTANCE;
+		//System.out.println(f.print());
+		f.print();
+	}
+}
 
+enum Foo{
+	INSTANCE;
+	public Foo getInstance(){
+		return INSTANCE;
+	}
+	public void print(){
+		System.out.println("hello foo!");
+		
+	}
 }
